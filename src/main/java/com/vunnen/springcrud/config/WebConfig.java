a1a5cf8ea1d91972ef10df46bbe28107af9ebd99
Niyaz <niazsajdullin@gmail.com>
2025-05-11 13:18:57 +0500

    add css resource handler

--- src/main/java/com/vunnen/springcrud/config/WebConfig.java
+++ src/main/java/com/vunnen/springcrud/config/WebConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring6.SpringTemplateEngine;
@@ -54,5 +55,8 @@ public class WebConfig implements WebMvcConfigurer {
         registry.viewResolver(resolver);
     }
 
-
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/styles/**").addResourceLocations("/styles/");
+    }
 }
